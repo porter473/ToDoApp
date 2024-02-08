7a0115ae178e3eb6832a5c01841a66bd313816a7
Aman Kumar Singh <aman.singh@theporter.in>
2024-02-08 19:26:13 +0530

    removing test cases

--- src/test/kotlin/com/example/service/RectangleTest.kt
+++ src/test/kotlin/com/example/service/RectangleTest.kt
@@ -5,12 +5,7 @@ import kotlin.test.assertEquals
 
 class RectangleTest {
 
-    @Test
-    fun `test rectangle area calculation`() {
-        val rectangle = Rectangle(10.0, 5.0)
-        val expectedArea = 50.0
-        assertEquals(expectedArea, rectangle.area(), "The area of the rectangle is incorrect")
-    }
+
 
 
 }
