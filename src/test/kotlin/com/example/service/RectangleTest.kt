40c891b1640ef3133376a0c40caf3672edea37e0
Aman Kumar Singh <aman.singh@theporter.in>
2024-02-26 00:06:34 +0530

    choreL adding test case

--- src/test/kotlin/com/example/service/RectangleTest.kt
+++ src/test/kotlin/com/example/service/RectangleTest.kt
@@ -1,11 +1,15 @@
 package com.example.service
 
+import java.awt.Rectangle
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class RectangleTest {
 
-
-
-
+    @Test
+    fun `test rectangle area calculation`() {
+        val rectangle = Rectangle(10.0, 2.0)
+        val expectedArea = 20.0
+        assertEquals(expectedArea, rectangle.area(), "The area of the rectangle is incorrect")
+    }
 }
